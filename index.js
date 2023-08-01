acbc6c3dfae8e3132cf97fd5ee74a44d7101df3d
Nacho <ignafernandez92@gmail.com>
2023-08-01 13:46:57 -0300

    chismen

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const userRoutes = require('./routes/userRoutes');
-const tournamentRoutes = require('./routes/tournamentRoutes');
+// Import the mysql package and create the database connection
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'padel.cwnvlmknuyto.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'padel123456',
+  database: '',
+});
+connection.connect();
 
-const port = 3000;
-
+// Event listener for the 'connect' event
+connection.on('connect', () => {
+    console.log('Connected to the database!');
+  });
+  
+  // Event listener for the 'error' event
+  connection.on('error', (err) => {
+    console.error('Error connecting to the database:', err.message);
+  });
+  
+  // Connect to the database
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+    } else {
+      console.log('Database connection successful!');
+    }
+  });
 
 const nacho = 1 + 2
 
 /*testing nuevo*/
+const app = express();
+const port = 3306;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
