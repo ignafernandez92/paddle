9f4c8e2ce3fdb1b35860ee81f1d032e2a2262060
Nacho <ignafernandez92@gmail.com>
2023-08-08 09:48:33 -0300

    cambios en rutas

--- routes/tournamentRoutes.js
+++ routes/tournamentRoutes.js
@@ -25,17 +25,15 @@ router.post('/', (req, res) => {
   try {
     // Obtener los datos del nuevo torneo desde el cuerpo de la solicitud
     const { 
-      nombreTorneo,
-      categorias,
-      genero,
-      fechaInicio,
-      fechaFin,
-      clubesParticipantes,
-      precioInscripcion,
-      descripcion,
-      premios,
+      nombre,
+      description,
+      start_date,
+      end_date,
+      creator_id,
+      prize,
     } = req.body;
 
+    const currentTimestamp = new Date().toISOString();
     // Configurar la conexión a la base de datos utilizando las variables de entorno
     const connection = mysql.createConnection({
       host: process.env.DB_HOST,
@@ -48,14 +46,14 @@ router.post('/', (req, res) => {
     connection.connect();
 
     // Insertar el nuevo torneo en la base de datos
-    const query = `INSERT INTO tournaments (nombreTorneo, categorias, genero, fechaInicio, fechaFin, clubesParticipantes, precioInscripcion, descripcion, premios)
-                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-    const values = [nombreTorneo, categorias, genero, fechaInicio, fechaFin, clubesParticipantes, precioInscripcion, descripcion, premios];
+    const query = `INSERT INTO tournaments (name, description, start_date, end_date, creator_id, prize, created_at, updated_at)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [nombre, description, start_date, end_date, creator_id, prize, currentTimestamp, currentTimestamp];
     connection.query(query, values, (error, results) => {
       if (error) {
         res.status(400).json({ message: 'Error al registrar el torneo', error });
       } else {
-        res.status(201).json({ message: 'Torneo registrado correctamente', tournament: { id: results.insertId, nombreTorneo, categorias, genero, fechaInicio, fechaFin, clubesParticipantes, precioInscripcion, descripcion, premios } });
+        res.status(201).json({ message: 'Torneo registrado correctamente', tournament: { id: results.insertId, nombre, description, creator_id, start_date, end_date, prize} });
       }
     });
 
