be54cfb3e9e6ae6e16464169ed21a48b2995fbf6
Nacho <ignafernandez92@gmail.com>
2023-08-22 08:47:33 -0300

    angular test

--- padel-app/src/app/app.module.ts
+++ padel-app/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- padel-app/src/app/registration/registration.component.ts
+++ padel-app/src/app/registration/registration.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.sass']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
 
+  constructor(private apiService: ApiService, private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      f_name: ['', Validators.required],
+      l_name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      dni: ['', Validators.required],
+      date_of_birth: ['', Validators.required],
+      role: ['player', Validators.required] // Default value
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      const userData = this.registrationForm.value;
+      this.apiService.registerUser(userData).subscribe(
+        (response) => {
+          console.log('User registered:', response);
+          // Handle successful registration, show a message, or redirect to login page
+        },
+        (error) => {
+          console.error('Error registering user:', error);
+          // Handle error, show an error message, etc.
+        }
+      );
+    }
+  }
 }
