341eae168392a83427fab5156932450922e7b708
aguscabrerapauli <agus_cabrera08@hotmail.com>
2023-08-31 12:54:17 -0300

    angular rutas

--- padel-app/src/app/features/clubs/clubs-routing.module.ts
+++ padel-app/src/app/features/clubs/clubs-routing.module.ts
@@ -5,7 +5,17 @@ import { ClubsPageComponent } from './pages/clubs-page/clubs-page.component';
 const routes: Routes = [{
     path: '',
     component: ClubsPageComponent
-}];
+},
+{
+    path: 'clubs/:id',
+    component: ClubsPageComponent
+},
+{
+    path: '**',
+    component: ClubsPageComponent
+}
+
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
