ee37df67141aa4d4fbc468e14d574ff01d7e381c
aguscabrerapauli <agus_cabrera08@hotmail.com>
2023-08-31 15:02:33 -0300

    1

--- padel-app/src/app/shared/shared.module.ts
+++ padel-app/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
