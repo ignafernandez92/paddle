e64e0afe6c92a5cb733ecdb5dc01078e1a206f81
aguscabrerapauli <agus_cabrera08@hotmail.com>
2023-10-19 08:22:52 -0300

    new tournament header

--- padel-app/src/app/new-tournament/new-tournament.component.ts
+++ padel-app/src/app/new-tournament/new-tournament.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-new-tournament',
   templateUrl: './new-tournament.component.html',
   styleUrls: ['./new-tournament.component.css']
 })
-export class NewTournamentComponent {
+export class NewTournamentComponent implements OnInit {
+  errorMessage: string = '';
+  constructor(private authService: AuthService, private navigationService: NavigationService) {}
+
+  onNavigate(feature: string) {
+    console.log('onNavigate called with feature:', feature);
+    
+    if (this.authService.isAuthenticated()) {
+      console.log(`User is authenticated, navigating to ${feature}`);
+      this.navigationService.navigateTo(feature);
+    } else {
+      console.log('User is not authenticated');
+      this.navigationService.navigateTo('login'); 
+      this.errorMessage = `You need to be authenticated to access the ${feature}.`;
+    }
+  }
+  ngOnInit(): void {
+    console.log('NewTournamentComponent - ngOnInit');
+  }
+
+  clearErrorMessage() {
+      this.errorMessage = '';
+    }
   
   }
 
 
 
+  
+  
